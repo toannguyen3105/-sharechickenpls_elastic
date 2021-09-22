ebe6e579c893f7d21ac960a8e75499567b5f62fa
tony <nguyenhuytoan1994@outlook.com>
2021-09-23 01:04:30 +0700

    Init project 2

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 
 from resources.indice import IndiceList, IndiceItem
 from resources.comic import ComicList, ComicItem, ComicItemCreate
+from resources.ssadoc import SsadocItem, SsadocList, SsadocItemCreate
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +16,9 @@ api.add_resource(IndiceList, '/indices')
 api.add_resource(ComicItem, '/comics/<string:item_id>')
 api.add_resource(ComicItemCreate, '/comics')
 api.add_resource(ComicList, '/comics')
+api.add_resource(SsadocItem, '/ssadocs/<string:item_id>')
+api.add_resource(SsadocItemCreate, '/ssadocs')
+api.add_resource(SsadocList, '/ssadocs')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
